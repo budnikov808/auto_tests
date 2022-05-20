fb521bacbec2a6440e4f37f21e7d9ed73dc21e50
Будников Андрей <104449759+budnikov808@users.noreply.github.com>
2022-05-20 10:37:48 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,16 @@
 sudo apt update
 
 # installing python
-#sudo apt -y install python3.8 python3.8-venv &&
-#python3 -m venv env &&
+sudo apt -y install python3.8 python3.8-venv &&
+python3 -m venv env &&
 source ./env/bin/activate &&
 pip3 install -r requirements.txt &&
 deactivate
 
 # installing geckodriver for selenium
-#mkdir -p ~/geckodriver && cd ~/geckodriver
-#sudo apt install wget &&
-#wget -O geckodriver.tar.gz https://github.com/mozilla/geckodriver/releases/download/v0.31.0/geckodriver-v0.31.0-linux64.tar.gz &&
-#tar xzvf geckodriver.tar.gz &&
-#sudo chmod +x geckodriver &&
-#zzzsudo mv geckodriver /usr/local/bin/
+mkdir -p ~/geckodriver && cd ~/geckodriver
+sudo apt install wget &&
+wget -O geckodriver.tar.gz https://github.com/mozilla/geckodriver/releases/download/v0.31.0/geckodriver-v0.31.0-linux64.tar.gz &&
+tar xzvf geckodriver.tar.gz &&
+sudo chmod +x geckodriver &&
+sudo mv geckodriver /usr/local/bin/
